1f96c1487ad51ccc62b9d083a87ee64294ee102b
justin <justin@garofolo.net>
2025-03-27 21:26:06 -0400

    test wh 1

--- mystipy.py
+++ mystipy.py
@@ -7,10 +7,14 @@ from io import BytesIO
 default_key = "mystipy"
 default_salt_sz = 16
 
+
 def xor(data, key):
     return bytes(a ^ b for a, b in zip(data, key * (len(data) // len(key) + 1)))
 
-def obfuscate(input_data, key, salt_sz = default_salt_sz, hex = True):
+# MIMIR WH TEST [DIFF 1]
+
+
+def obfuscate(input_data, key, salt_sz=default_salt_sz, hex=True):
     salt = os.urandom(salt_sz)
     input_data = salt + input_data
     encoded_content = base64.b64encode(input_data)
@@ -24,7 +28,8 @@ def obfuscate(input_data, key, salt_sz = default_salt_sz, hex = True):
         return gzipped_data.hex().encode()
     return gzipped_data
 
-def deobfuscate(input_data, key, salt_sz = default_salt_sz, hex = True):
+
+def deobfuscate(input_data, key, salt_sz=default_salt_sz, hex=True):
     if hex:
         input_data = bytes.fromhex(input_data.decode())
     buffer = BytesIO(input_data)
@@ -35,27 +40,38 @@ def deobfuscate(input_data, key, salt_sz = default_salt_sz, hex = True):
     decoded_data = base64.b64decode(encoded_content)
     return decoded_data[salt_sz:]
 
+
 def read_file(file_path):
     with open(file_path, 'rb') as file:
         return file.read()
+
 
 def write_file(file_path, data):
     with open(file_path, 'wb') as file:
         file.write(data)
 
+
 # function aliases
 mystipy = obfuscate
 demystipy = deobfuscate
 
+
 def main():
-    parser = argparse.ArgumentParser(description = "Obfuscate or deobfuscate the contents of the file in a very simple way.")
-    parser.add_argument("input", type = str, nargs = "?", help = "Input file path.")
-    parser.add_argument("output", type = str, nargs = "?", help = "Output file path.")
-    parser.add_argument("-p", "--prompt", action = "store_true", help = "Prompt mode. If provided, you will be prompted for the arguments you don't explicitly set.")
-    parser.add_argument("-r", "--reverse", action = "store_true", help = "If provided, the protection will be reversed. (Deobfuscate)")
-    parser.add_argument("-k", "--key", type = str, default = default_key, help = "XOR key for encryption/decryption. Must be a valid byte string.")
-    parser.add_argument("-s", "--salt", type = int, default = default_salt_sz, help = "Number of bytes to use in salt. (Default: 16)")
-    parser.add_argument("--binary", action = "store_true", help = "Disable hexadecimal encoding/decoding, meaning the data will be (or is) compressed binary data.")
+    parser = argparse.ArgumentParser(
+        description="Obfuscate or deobfuscate the contents of the file in a very simple way.")
+    parser.add_argument("input", type=str, nargs="?", help="Input file path.")
+    parser.add_argument("output", type=str, nargs="?",
+                        help="Output file path.")
+    parser.add_argument("-p", "--prompt", action="store_true",
+                        help="Prompt mode. If provided, you will be prompted for the arguments you don't explicitly set.")
+    parser.add_argument("-r", "--reverse", action="store_true",
+                        help="If provided, the protection will be reversed. (Deobfuscate)")
+    parser.add_argument("-k", "--key", type=str, default=default_key,
+                        help="XOR key for encryption/decryption. Must be a valid byte string.")
+    parser.add_argument("-s", "--salt", type=int, default=default_salt_sz,
+                        help="Number of bytes to use in salt. (Default: 16)")
+    parser.add_argument("--binary", action="store_true",
+                        help="Disable hexadecimal encoding/decoding, meaning the data will be (or is) compressed binary data.")
 
     args = parser.parse_args()
 
@@ -78,17 +94,21 @@ def main():
     if args.prompt:
         # prompt mode - prompt for any values they didn't explicitly set
         input_path = input_path if input_path else input("Input file path: ")
-        output_path = output_path if output_path else input("Output file path: ")
+        output_path = output_path if output_path else input(
+            "Output file path: ")
         if not reverse:
-            _reverse = input("Are you reversing the obfuscation? [y/n, default = n]: ").lower()
+            _reverse = input(
+                "Are you reversing the obfuscation? [y/n, default = n]: ").lower()
             reverse = len(_reverse) > 0 and _reverse[:1].lower() == "y"
         if not binary:
-            _binary = input("Should the output be hex encoded? [y/n, default = y]: ").lower()
+            _binary = input(
+                "Should the output be hex encoded? [y/n, default = y]: ").lower()
             binary = len(_binary) > 0 and _binary[:1].lower() == "n"
         if key == default_key:
-            _key = input("Enter a custom encryption key, or leave blank to use default: ")
+            _key = input(
+                "Enter a custom encryption key, or leave blank to use default: ")
             key = _key if len(_key) else key
-    
+
     key = key.encode()
     input_data = read_file(input_path)
 
@@ -102,6 +122,7 @@ def main():
         print(f"File has been {verb}d.")
     except Exception as ex:
         print(f"Failed to {verb} data.\n{ex}")
+
 
 if __name__ == "__main__":
     main()
